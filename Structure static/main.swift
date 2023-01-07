11323ff664ebbd430fdcfa81e206903caac0b430
Showaa-06 <115485438+Showaa-06@users.noreply.github.com>
2023-01-08 00:22:11 +0900

    first commit

--- Structure static/main.swift
+++ Structure static/main.swift
@@ -7,5 +7,21 @@
 
 import Foundation
 
-print("Hello, World!")
+struct MyPassword {
+    let IPhone = "0611"
+    static let Macbook = "6011"
+    
+    func instanceMethod() {
+    }
+    static func typeMethod() {
+    }
+}
+
+let myPassword = MyPassword()
+print(myPassword.IPhone)//インスタンス後を指定
+myPassword.instanceMethod()
+
+
+print(MyPassword.Macbook)//インスタンス前を指定
+MyPassword.typeMethod()
 
